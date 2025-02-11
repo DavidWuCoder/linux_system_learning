72a4abd7aa167f5e5d1bd1da0c9d32b469a73512
Infinite <468588126@qq.com>
2025-02-11 23:10:29 +0800

    test wait func

--- test_02_11/proc.c
+++ test_02_11/proc.c
@@ -1,26 +1,53 @@
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main() {
-  // printf("hello world\n");
+  pid_t id = fork();
 
-  // FILE *fp = fopen("log.txt", "r");
-  // if (fp == NULL) {
-  //   return errno;
-  // }
-  // // 读取
-  // fclose(fp);
+  if (id == 0) {
+    // child
+    int cnt = 5;
+    while (cnt) {
+      printf("我是一个子进程，pid: %d, ppid: %d \n", getpid(), getppid());
+      sleep(1);
+      cnt--;
+    }
+    exit(0);
+  }
 
-  // int i = 0;
-  // for (int i = 0; i < 200; i++) {
-  //   printf("%d -> %s \n", i, strerror(i));
-  // }
+  // parent
+  // sleep(100);
+  pid_t rid = wait(NULL);
+  if (rid > 0) {
+    printf("wait success, rid: %d\n", rid);
+  }
 
-  // return errno;
-  //
-
-  int a = 10;
-  a /= 0;
-  return 89;
+  return 0;
 }
+
+// int main() {
+//   // printf("hello world\n");
+//
+//   // FILE *fp = fopen("log.txt", "r");
+//   // if (fp == NULL) {
+//   //   return errno;
+//   // }
+//   // // 读取
+//   // fclose(fp);
+//
+//   // int i = 0;
+//   // for (int i = 0; i < 200; i++) {
+//   //   printf("%d -> %s \n", i, strerror(i));
+//   // }
+//
+//   // return errno;
+//   //
+//
+//   int a = 10;
+//   a /= 0;
+//   return 89;
+// }
