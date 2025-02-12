1b78175728b4e6ecb5633040dba62271e966f581
Infinite <468588126@qq.com>
2025-02-12 16:31:01 +0800

    test None Block wait

--- test_02_11/proc.c
+++ test_02_11/proc.c
@@ -5,8 +5,35 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+typedef void (*func_t)();
+
+#define NUM 5
+func_t handlers[NUM];
+
+void Download() { printf("我是一个下载任务！\n"); }
+void Flush() { printf("我是一个刷新任务！\n"); }
+void Log() { printf("我是一个记录日志任务！\n"); }
+
+void registerHandlers(func_t h[], func_t f) {
+  int i = 0;
+  for (; i < NUM; i++) {
+    if (h[i] == NULL) {
+      break;
+    }
+  }
+  if (i == NUM) {
+    return;
+  }
+  h[i] = f;
+  h[i + 1] = NULL;
+}
+
 int main() {
   pid_t id = fork();
+
+  registerHandlers(handlers, Download);
+  registerHandlers(handlers, Flush);
+  registerHandlers(handlers, Log);
 
   if (id == 0) {
     // child
@@ -27,6 +54,11 @@ int main() {
              WEXITSTATUS(status), status & 0x7f);
       break;
     } else if (rid == 0) {
+      // 函数指针回调处理
+      int i = 0;
+      for (; handlers[i]; i++) {
+        handlers[i]();
+      }
       printf("本轮调用结束，子进程没有退出\n");
       sleep(1);
     } else {
