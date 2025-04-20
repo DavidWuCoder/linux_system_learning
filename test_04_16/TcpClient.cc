31af760ad6405f72244a11e3946cc0fa91961f21
Infinite <468588126@qq.com>
2025-04-20 10:42:05 +0800

    client

--- test_04_16/TcpClient.cc
+++ test_04_16/TcpClient.cc
@@ -1,5 +1,6 @@
 #include "Socket.hpp"
 #include "Common.hpp"
+#include "Protocol.hpp"
 #include <iostream>
 #include <string>
 #include <memory>
@@ -21,12 +22,51 @@ int main(int argc, char *argv[])
     }
     std::string server_ip = argv[1];
     uint16_t server_port = std::stoi(argv[2]);
-    std::unique_ptr<Socket> client = std::make_unique<TcpSocket>();
+    std::shared_ptr<Socket> client = std::make_unique<TcpSocket>();
     client->BuildTcpClientSocketMethod();
 
-    if(client->Connect(server_ip, server_port) == 0)
+    if(client->Connect(server_ip, server_port) != 0)
     {
         //成功
-        
+        std::cerr << "connect error" << std::endl;
+        exit(CONNECT_ERR);
+    }
+    std::unique_ptr<Protocol> protocol = std::make_unique<Protocol>([](Request &req)->Response{
+        return Response();
+    });
+    std::string resp_buffer;
+    while (true)
+    {
+        // 1.
+        int x, y;
+        char oper;
+        std::cout << "Please Enter x # " << std::endl;
+        std::cin >> x;
+        std::cout << "Please Enter operator # " << std::endl;
+        std::cin >> oper;
+        std::cout << "Please Enter y # " << std::endl;
+        std::cin >> y;
+
+        // 2.
+        std::string req_str = protocol->BuildRequestString(x, y, oper); 
+        std::cout << "===========encode req string ==============" << std::endl;
+        std::cout << req_str << std::endl;
+        std::cout << "===========================================" <<std::endl;
+
+
+        // 3.
+        client->Send(req_str);
+
+
+        //4. 
+        Response resp;
+        bool res = protocol->GetResponse(client, resp_buffer, &resp);
+        if (res == false)
+        {
+            break;
+        }
+
+        // 5.
+        resp.ShowResult();
     }
 }
