e2fa58fdc97b730b6b1a6d434340e42df05f3bb3
DavidWuCoder <468588126@qq.com>
2025-06-12 22:32:19 +0800

    epollserver

--- test_06_12/PollServer/Util.hpp
+++ test_06_12/PollServer/Util.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 class Util
 {
 public:
-    static bool ReadFileContent(const std::string &filename, std::string* out)
+    static bool ReadFileContent(const std::string &filename, std::string *out)
     {
         // version1: 默认以文本形式读取
         // std::ifstream in(filename);
@@ -43,17 +43,19 @@ public:
         return true;
     }
 
-    // static bool ReadFileContent(const std::string &filename, std::string *out)
+    // static bool ReadFileContent(const std::string &filename, std::string
+    // *out)
     // {
-    //     std::ifstream in(filename, std::ios::binary | std::ios::ate); // 直接定位到文件末尾
+    //     std::ifstream in(filename, std::ios::binary | std::ios::ate); //
+    //     直接定位到文件末尾
 
     //     if (!in.is_open())
     //     {
     //         return false;
     //     }
 
-    //     const std::streamsize filesize = in.tellg(); // 直接通过流获取文件大小
-    //     in.seekg(0, std::ios::beg);
+    //     const std::streamsize filesize = in.tellg(); //
+    //     直接通过流获取文件大小 in.seekg(0, std::ios::beg);
 
     //     if (filesize <= 0)
     //     {
@@ -62,13 +64,15 @@ public:
 
     //     out->clear();
     //     out->resize(filesize);
-    //     in.read(reinterpret_cast<char *>(out->data()), filesize); // C++17正确访问
+    //     in.read(reinterpret_cast<char *>(out->data()), filesize); //
+    //     C++17正确访问
 
     //     // 处理可能的读入不完整
     //     return in.good() && (filesize == in.gcount());
     // }
 
-    static bool ReadOneLine(std::string &bigstr, std::string *out, const std::string &sep)
+    static bool ReadOneLine(std::string &bigstr, std::string *out,
+                            const std::string &sep)
     {
         auto pos = bigstr.find(sep);
         if (pos == std::string::npos)
